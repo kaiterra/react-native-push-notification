29dcfdac6b56b984ba423d7f5410747e5f38d673
Michael Hasak <mphasak@gmail.com>
2018-08-01 16:00:08 +0800

    rename parameters and add placeholder event handlers

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/AliyunMessageReceiver.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/AliyunMessageReceiver.java
@@ -26,17 +26,44 @@ public class AliyunMessageReceiver extends MessageReceiver {
         params.putString("title", cPushMessage.getTitle());
         sendEvent(AliyunPushModule.getReactContext(), "onMessage", params);
     }
+
     @Override
-    protected void onNotification(Context context, String s, String s1, Map<String, String> map) {
-        super.onNotification(context, s, s1, map);
+    protected void onNotification(Context context, String title, String body, Map<String, String> map) {
+        super.onNotification(context, title, body, map);
         WritableMap params = Arguments.createMap();
-        params.putString("content", s1);
-        params.putString("title", s);
+        params.putString("content", body);
+        params.putString("title", title);
         for (Map.Entry<String, String> entry: map.entrySet()) {
             params.putString(entry.getKey(), entry.getValue());
         }
         sendEvent(AliyunPushModule.getReactContext(), "onNotification", params);
     }
+
+    @Override
+    protected void onNotificationOpened(Context var1, String var2, String var3, String var4) {
+        Log.i(MessageReceiver.TAG, "notification opened");
+    }
+
+    @Override
+    protected void onNotificationRemoved(Context var1, String var2) {
+        Log.i(MessageReceiver.TAG, "notification removed");
+    }
+
+    @Override
+    protected void onNotificationReceivedInApp(Context var1, String var2, String var3, Map<String, String> var4, int var5, String var6, String var7) {
+        Log.i(MessageReceiver.TAG, "notification received");
+    }
+
+    @Override
+    protected void onNotificationClickedWithNoAction(Context var1, String var2, String var3, String var4) {
+        Log.i(MessageReceiver.TAG, "notification clicked with no action");
+    }
+
+    @Override
+    protected void onConnectionStatusChanged(boolean var1) {
+        Log.i(MessageReceiver.TAG, "connection status changed");
+    }
+
     private void sendEvent(ReactContext context, String eventName, @Nullable WritableMap params) {
         if (context == null) {
             Log.i(MessageReceiver.TAG, "reactContext==null");
